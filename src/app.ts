09ba160688bf6cce0ebd57bd437bfef07dc2eadf
kizunas <8s6s7e@gmail.com>
2024-05-25 15:56:36 +0900

    DOM要素の取得（フォームの表示）

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  element: HTMLElement;
+  element: HTMLFormElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // テンプレート要素への参照
@@ -13,12 +13,17 @@ class ProjectInput {
     // this.templateElement.contentを渡すことでテンプレートタグの内側にあるタグ（formタグ)を参照する。
     // importNodeの第二引数はhtmlタグの最初の階層だけではなく、その下の階層も含めてインポートするというもの
     const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement; // formタグを指す
+    this.attach()
   }
 
   private attach() { // hostElementに要素を追加する。
     // insertAdjacentElementはブラウザのJavaScriptで提供されているもの。HTML文書の既存の要素に対して新しい要素を特定の位置に挿入するためのDOMメソッドです。これにより、指定した要素の前後や内部の特定の位置に新しい要素を簡単に追加することができます。
     // 最初の引数はどこに追加するかのオプション。
     // afterbeginは開始タグの後ろ、afterendは終了タグの後ろ、beforebeginは開始タグの前、beforeendは終了タグの前。
-    this.hostElement.insertAdjacentElement('afterbegin')
+    // 第二引数に挿入したい要素を渡す
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
   }
 }
+
+const prjInput = new ProjectInput();
