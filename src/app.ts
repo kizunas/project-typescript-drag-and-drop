791a38b069a3e18e5978790c9a9fd8e2ec76a175
kizunas <8s6s7e@gmail.com>
2024-05-25 15:48:08 +0900

    importNodeとinsertAdjacentElement

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; // テンプレート要素への参照
+    this.hostElement = document.getElementById('app')! as HTMLDivElement; // templateを表示する親要素への参照
+    
+
+    // テンプレートのインポートをする。
+    // importNodeメソッドを使用する。別のドキュメントからノード（要素、属性、テキストなど）を現在のドキュメントにインポートするために使用されるDOMメソッドです。このメソッドは、ノードをコピーして新しいドキュメントに追加するのに便利です。
+    // this.templateElement.contentを渡すことでテンプレートタグの内側にあるタグ（formタグ)を参照する。
+    // importNodeの第二引数はhtmlタグの最初の階層だけではなく、その下の階層も含めてインポートするというもの
+    const importedNode = document.importNode(this.templateElement.content, true)
+  }
+
+  private attach() { // hostElementに要素を追加する。
+    // insertAdjacentElementはブラウザのJavaScriptで提供されているもの。HTML文書の既存の要素に対して新しい要素を特定の位置に挿入するためのDOMメソッドです。これにより、指定した要素の前後や内部の特定の位置に新しい要素を簡単に追加することができます。
+    // 最初の引数はどこに追加するかのオプション。
+    // afterbeginは開始タグの後ろ、afterendは終了タグの後ろ、beforebeginは開始タグの前、beforeendは終了タグの前。
+    this.hostElement.insertAdjacentElement('afterbegin')
+  }
+}
